5fcca40761bc8bff098c030fb3f1d3f5aee6de86
Kirat Virmani <kirat19984@gmail.com>
2019-08-03 17:55:59 +0530

    Scenes Load

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public SceneAsset[] Levels;
+
+    public int CurrentLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentLevel = 0;
+        SceneManager.LoadScene(Levels[CurrentLevel].name, LoadSceneMode.Additive);
+
+        StartCoroutine(SceneChange(3));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNextScene(LoadSceneMode mode)
+    {
+        if (Levels.Length > CurrentLevel + 1)
+        {
+            CurrentLevel++;
+            SceneManager.LoadScene(Levels[CurrentLevel].name, LoadSceneMode.Additive);
+        }
+        else
+        {
+            Debug.Log("No Scenes to load");
+        }
+    }
+    
+    public IEnumerator SceneChange(int Delay = 0)
+    {
+        yield return new WaitForSeconds(Delay);
+        LoadNextScene(LoadSceneMode.Additive);
+    }
+}
---
+++ Assets/Scripts/Scean Change/GetCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // get camera here
+    }
+}
