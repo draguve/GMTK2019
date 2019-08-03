6dd2dff1b01d5615e53a88dfca278ab35dbfcfbe
Kirat Virmani <kirat19984@gmail.com>
2019-08-04 04:12:01 +0530

    Flare Works

--- Assets/ParticleOnAnimation.cs
+++ Assets/ParticleOnAnimation.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
 
 public class ParticleOnAnimation : MonoBehaviour
 {
     public GameObject FlareSmoke;
     private ParticleSystem smokeParticles;
+    public GameObject Light;
+    private Light2D FlareLight;
+    public AnimationCurve lightCurve;
+    public float FlareDuration;
+
     // Start is called before the first frame update
     void Start()
     {
         smokeParticles = FlareSmoke.GetComponent<ParticleSystem>();
+        FlareLight = Light.GetComponent<Light2D>();
     }
 
     // Update is called once per frame
@@ -18,5 +26,12 @@ public class ParticleOnAnimation : MonoBehaviour
         FlareSmoke.SetActive(true);
         smokeParticles.Play();
         Debug.Log("Smoke played");
+    }
+
+    void PlayLight()
+    {
+        DOTween.To(x => FlareLight.intensity = x, 0, 12, FlareDuration).SetEase(lightCurve);
+        //FlareLight.intensity = 1;
+        Debug.Log("PLayed Light");
     }
 }
